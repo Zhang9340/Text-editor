5c33a23b37d3835ee7ee6eee77c0d0023c2a7173
Grace <tl544@cornell.edu>
2023-03-21 23:44:03 -0400

    Implemented move_cursor; Removed module Text; Renamed insert_str to replace_str.

--- src/text_editor.ml
+++ src/text_editor.ml
@@ -1,117 +1,139 @@
 (* open Graphics *)
-module Text = struct
-  type editor_state = {
-    text : string list;
-    cursor_pos : int * int;
-    selection_start : int option * int option;
-    selection_end : int option * int option;
+
+type editor_state = {
+  text : string list;
+  cursor_pos : int * int;
+  selection_start : int option * int option;
+  selection_end : int option * int option;
+}
+
+let create_editor_state () =
+  {
+    text = [];
+    cursor_pos = (0, 0);
+    selection_start = (None, None);
+    selection_end = (None, None);
   }
 
-  let create_editor_state () =
-    {
-      text = [];
-      cursor_pos = (0, 0);
-      selection_start = (None, None);
-      selection_end = (None, None);
-    }
+let get_text state = state.text
 
-  let get_text state = state.text
+let load_file filename =
+  let chan = open_in filename in
+  let rec loop acc =
+    try
+      let line = input_line chan in
+      loop (line :: acc)
+    with End_of_file ->
+      close_in chan;
+      {
+        text = List.rev acc;
+        cursor_pos = (0, 0);
+        selection_start = (None, None);
+        selection_end = (None, None);
+      }
+  in
+  loop []
 
-  let load_file filename =
-    let chan = open_in filename in
-    let rec loop acc =
-      try
-        let line = input_line chan in
-        loop (line :: acc)
-      with End_of_file ->
-        close_in chan;
-        {
-          text = List.rev acc;
-          cursor_pos = (0, 0);
-          selection_start = (None, None);
-          selection_end = (None, None);
-        }
-    in
-    loop []
+let text_format state =
+  let lst = get_text state in
+  let rec loop ls =
+    match ls with
+    | hd :: tl -> if tl == [] then hd else hd ^ "\n" ^ loop tl
+    | [] -> ""
+  in
+  loop lst
 
-  let text_format state =
-    let lst = get_text state in
-    let rec loop ls =
-      match ls with
-      | hd :: tl -> if tl == [] then hd else hd ^ "\n" ^ loop tl
-      | [] -> ""
-    in
-    loop lst
+let save_file state filename =
+  let chan = open_out filename in
+  List.iter (fun line -> output_string chan (line ^ "\n")) (get_text state);
+  close_out chan
 
-  let save_file state filename =
-    let chan = open_out filename in
-    List.iter (fun line -> output_string chan (line ^ "\n")) (get_text state);
-    close_out chan
+let rec sublist b e l =
+  if e <= b then []
+  else
+    match l with
+    | [] -> []
+    | h :: t ->
+        let tail = if e = 0 then [] else sublist (b - 1) (e - 1) t in
+        if b > 0 then tail else h :: tail
 
-  let rec sublist b e l =
-    if e <= b then []
-    else
-      match l with
-      | [] -> []
-      | h :: t ->
-          let tail = if e = 0 then [] else sublist (b - 1) (e - 1) t in
-          if b > 0 then tail else h :: tail
+let update_in_one_row state s start_pos_r start_pos_c end_pos_c =
+  let stext = List.nth (get_text state) start_pos_r in
+  let prefix = String.sub stext 0 start_pos_c in
+  let suffix =
+    String.sub stext end_pos_c (String.length stext - end_pos_c - 1)
+  in
+  prefix ^ s ^ suffix
 
-  let update_in_one_row state s start_pos_r start_pos_c end_pos_c =
-    let stext = List.nth (get_text state) start_pos_r in
-    let prefix = String.sub stext 0 start_pos_c in
-    let suffix =
-      String.sub stext end_pos_c (String.length stext - end_pos_c - 1)
-    in
-    prefix ^ s ^ suffix
+let update_row state s =
+  match (state.selection_start, state.selection_end) with
+  | (Some start_pos_r, Some start_pos_c), (Some end_pos_r, Some end_pos_c) ->
+      if end_pos_r - start_pos_r = 0 then
+        update_in_one_row state s start_pos_r start_pos_c end_pos_c
+      else " " (* TODO: haven't implemented switch line change *)
+  | _ -> failwith "fail to update row"
 
-  let update_row state s =
-    match (state.selection_start, state.selection_end) with
-    | (Some start_pos_r, Some start_pos_c), (Some end_pos_r, Some end_pos_c) ->
-        if end_pos_r - start_pos_r = 0 then
-          update_in_one_row state s start_pos_r start_pos_c end_pos_c
-        else " " (* TODO: haven't implemented switch line change *)
-    | _ -> failwith "fail to update row"
+let update_all_rows state r =
+  match (state.selection_start, state.selection_end) with
+  | (Some start_pos_r, _), (Some end_pos_r, _) ->
+      let prefix = sublist 0 (start_pos_r - 1) (get_text state) in
+      let suffix =
+        sublist end_pos_r
+          (List.length (get_text state) - end_pos_r)
+          (get_text state)
+      in
+      prefix @ [ r ] @ suffix
+  | _ -> failwith "fail to update column"
 
-  let update_all_rows state r =
-    match (state.selection_start, state.selection_end) with
-    | (Some start_pos_r, _), (Some end_pos_r, _) ->
-        let prefix = sublist 0 (start_pos_r - 1) (get_text state) in
-        let suffix =
-          sublist end_pos_r
-            (List.length (get_text state) - end_pos_r)
-            (get_text state)
-        in
-        prefix @ [ r ] @ suffix
-    | _ -> failwith "fail to update column"
+let option_tuple_get_int t =
+  match t with
+  | Some a, Some b -> (a, b)
+  | _ -> failwith "internal error"
 
-  let option_tuple_get_int t =
-    match t with
-    | Some a, Some b -> (a, b)
-    | _ -> failwith "internal error"
+let replace_str state s =
+  let text = s |> update_row state |> update_all_rows state in
+  { state with text; cursor_pos = option_tuple_get_int state.selection_start }
 
-  let insert_str state s =
-    let text = s |> update_row state |> update_all_rows state in
-    { state with text; cursor_pos = option_tuple_get_int state.selection_start }
+let select_text state start_pos_r start_pos_c end_pos_r end_pos_c =
+  {
+    state with
+    selection_start = (Some start_pos_r, Some start_pos_c);
+    selection_end = (Some end_pos_r, Some end_pos_c);
+  }
 
-  let select_text state start_pos_r start_pos_c end_pos_r end_pos_c =
-    {
-      state with
-      selection_start = (Some start_pos_r, Some start_pos_c);
-      selection_end = (Some end_pos_r, Some end_pos_c);
-    }
-end
+let tuple_get_first t =
+  match t with
+  | r, _ -> r
+
+let tuple_get_second t =
+  match t with
+  | _, c -> c
+
+let move_cursor state (offset_r, offset_c) =
+  let new_pos_r =
+    max 0
+      (min (List.length state.text)
+         (tuple_get_first state.cursor_pos + offset_r))
+  in
+  let new_pos_c =
+    max 0
+      (min (List.length state.text)
+         (tuple_get_second state.cursor_pos + offset_c))
+  in
+  {
+    state with
+    cursor_pos = (new_pos_r, new_pos_c);
+    selection_start = (None, None);
+    selection_end = (None, None);
+  }
+
 (* let delete_char state = let text = match (state.selection_start,
    state.selection_end) with | Some start_pos, Some end_pos -> let prefix =
    sublist 0 start_pos state.text in let suffix = sublist end_pos (List.length
    state.text - end_pos) state.text in prefix @ suffix | _ -> let prefix =
    sublist 0 (state.cursor_pos - 1) state.text in let suffix = sublist
    state.cursor_pos (List.length state.text - state.cursor_pos) state.text in
-   prefix @ suffix in { state with text; cursor_pos = state.cursor_pos - 1 }
-
-   let move_cursor state offset = let new_pos = max 0 (min (List.length
-   state.text) (state.cursor_pos + offset)) in { state with cursor_pos =
-   new_pos; selection_start = (None, None); selection_end = (None, None); } *)
+   prefix @ suffix in { state with text; cursor_pos = state.cursor_pos - 1 } *)
 
 (* type t = { mutable x : int; mutable y : int; mutable width : int; mutable
    height : int; mutable bg_color : Graphics.color; mutable text_color :
