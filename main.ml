28e820f64b339a6337fb854f5127df83fe1071f6
Grace <tl544@cornell.edu>
2023-04-27 22:50:44 -0400

    display example success

--- main.ml
+++ main.ml
@@ -1,1 +1,27 @@
-let _ = Ui.test
+(* let _ = Ui.test *)
+(* open Notty *)
+open Notty_unix
+
+(* let square = "\xe2\x96\xaa" *)
+let example = [ "a ad "; "qw era sdf"; "caf d" ]
+let image = Ui.convert example
+(* let rec sierp n = if n > 1 then let ss = sierp (pred n) in I.(ss <-> (ss <|>
+   ss)) else I.(string A.(fg magenta) square |> hpad 1 0) *)
+
+(* let img (double, n) = let s = sierp n in if double then I.(s </> vpad 1 0 s)
+   else s *)
+
+let rec update t img =
+  Term.image t img;
+  loop t img
+
+and loop t img =
+  match Term.event t with
+  | `Key (`Enter, _) -> ()
+  | _ -> loop t img
+
+let t = Term.create ()
+
+let _ =
+  update t image;
+  Term.release t
--- src/ui/ui.ml
+++ src/ui/ui.ml
@@ -1,7 +1,7 @@
 open Notty
 open Notty_unix
 
-let example = [ "a"; "qwer"; "c" ]
+let example = [ "aas fa f da"; "qwer"; "ca d fa f" ]
 
 let string_to_image s =
   let len = String.length s in
@@ -9,7 +9,7 @@ let string_to_image s =
     if i < len then
       let ch = s.[i] in
       let ch_img = I.uchar (A.fg A.blue) (Uchar.of_char ch) 1 1 in
-      loop (i + 1) I.(img <-> ch_img)
+      loop (i + 1) I.(img <|> ch_img)
     else img
   in
   loop 0 I.empty
@@ -17,7 +17,11 @@ let string_to_image s =
 let rec convert (c : string list) =
   match c with
   | [] -> I.empty
-  | h :: t -> I.(string_to_image h <|> convert t)
+  (* | `Key (`ASCII ' ', _) -> I.empty *)
+  (* | `Key (`Arrow `Left,_) -> update t (double, max 1 (n - 1)) | `Key (`Arrow
+     `Right,_) -> update t (double, min 8 (n + 1)) | `Key (`ASCII ' ', _) ->
+     update t (not double, n) | `Resize _ -> update t state *)
+  | h :: t -> I.(string_to_image h <-> convert t)
 
 (* let () = let img = convert example in Term.image_size (Term.create ()) img |>
    Printf.printf "Image size: %d x %d\n%!"; Term.image (Term.create ()) img *)
